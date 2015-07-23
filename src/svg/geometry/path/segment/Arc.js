66d251e1f5d8bbd3c7a5899af4818107727d7f04
Khusamov Sukhrob <khusamov@yandex.ru>
2015-07-23 12:50:13 +0000

    Добавлен расчет длины сегмента Khusamov.svg.geometry.path.segment.Arc

--- src/svg/geometry/path/segment/Arc.js
+++ src/svg/geometry/path/segment/Arc.js
@@ -3,6 +3,11 @@ Ext.define("Khusamov.svg.geometry.path.segment.Arc", {
 	
 	extend: "Khusamov.svg.geometry.path.segment.Segment",
 	
+	requires: [
+		"Khusamov.svg.geometry.equation.Circular", 
+		"Khusamov.svg.geometry.Line"
+	],
+	
 	config: {
 		
 		radius: [0, 0],
@@ -64,6 +69,19 @@ Ext.define("Khusamov.svg.geometry.path.segment.Arc", {
 	onParamUpdate: function() {
 		var path = this.getPath();
 		if (path) path.fireEvent("update");
+	},
+	
+	getLength: function() {
+		var length = 0;
+		var radius = this.getRadius();
+		if (radius[0] == radius[1]) radius = radius[0];
+		if (Ext.isNumber(radius)) {
+			var center = Khusamov.svg.geometry.equation.Circular.findCenter(this.getFirstPoint(), this.getLastPoint(), radius);
+			var first = Ext.create("Khusamov.svg.geometry.Line", center, this.getFirstPoint()).toLinear();
+			var last = Ext.create("Khusamov.svg.geometry.Line", center, this.getLastPoint()).toLinear();
+			length = radius * first.angleTo(last);
+		}
+		return length;
 	},
 	
 	toString: function() {
