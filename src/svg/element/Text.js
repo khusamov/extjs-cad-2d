4fed6cc1c5d2b1f45ccded39bb9b0054aeb2e1ba
Khusamov Sukhrob <khusamov@yandex.ru>
2015-07-31 10:07:06 +0000

    Добавлен элемент Khusamov.svg.element.Text

---
+++ src/svg/element/Text.js
@@ -0,0 +1,73 @@
+
+Ext.define("Khusamov.svg.element.Text", {
+	
+	extend: "Khusamov.svg.element.Element",
+	
+	xtype: "khusamov-svg-element-text",
+    
+    isSvgText: true,
+    
+	type: "text",
+    
+    baseCls: Ext.baseCSSPrefix + "khusamov-svg-text",
+    
+	autoEl: {
+		tag: "text"
+	},
+	
+	config: {
+		
+		anchor: [0, 0]
+		
+	},
+	
+	/**
+	 * Ext.create("Khusamov.svg.element.Text", x, y, text);
+	 * Ext.create("Khusamov.svg.element.Text", Number[x, y], text);
+	 * Ext.create("Khusamov.svg.element.Text", Khusamov.svg.geometry.Point, text);
+	 */
+	constructor: function(config) {
+		var me = this;
+		
+		if (arguments.length > 1) {
+			config = (arguments.length == 3) ? {
+				anchor: [arguments[0], arguments[1]],
+				html: arguments[2]
+			} : {
+				anchor: arguments[0],
+				html: arguments[1]
+			};
+		}
+		
+		me.callParent([config]);
+	},
+	
+	afterRender: function() {
+		var me = this;
+		me.callParent(arguments);
+		me.getEl().set({
+			x: me.getAnchor().x(),
+			y: me.getAnchor().y()
+		});
+	}
+	
+}, function(Text) {
+	
+	Khusamov.svg.Element.override({
+		
+		statics: {
+			
+			/**
+			 * Создать элемент Текст.
+			 * @return {Khusamov.svg.element.Text}
+			 */
+			createText: function() {
+				return Text.create.apply(Text, arguments);
+			}
+			
+		}
+		
+	});
+	
+});
+
