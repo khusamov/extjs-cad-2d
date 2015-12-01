d837a6e062ea67a5208cf74e700020100917e713
Khusamov Sukhrob <khusamov@yandex.ru>
2015-12-01 21:44:00 +0300

    Мелкие правки + Watch

--- packages/local/khusamov-svg/src/geometry/Line.js
+++ packages/local/khusamov-svg/src/geometry/Line.js
@@ -24,6 +24,8 @@ Ext.define("Khusamov.svg.geometry.Line", {
 	 * Ext.create("Khusamov.svg.geometry.Line", [x1, y1], [x2, y2]);
 	 * Ext.create("Khusamov.svg.geometry.Line", Khusamov.svg.geometry.Point, Khusamov.svg.geometry.Point);
 	 * Ext.create("Khusamov.svg.geometry.Line", Khusamov.svg.geometry.Point[]);
+	 * Ext.create("Khusamov.svg.geometry.Line", Khusamov.svg.geometry.vector.Vector, Khusamov.svg.geometry.vector.Vector);
+	 * Ext.create("Khusamov.svg.geometry.Line", Khusamov.svg.geometry.vector.Vector[]);
 	 */
 	constructor: function(config) {
 		var me = this, args = arguments;
