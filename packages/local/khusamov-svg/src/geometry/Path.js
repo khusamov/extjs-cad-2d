cd47813387d93457860a95276f18b9c2006749b4
Khusamov Sukhrob <khusamov@yandex.ru>
2015-12-06 16:37:29 +0300

    Добавлено определение принадлежности точки многоугольнику

--- packages/local/khusamov-svg/src/geometry/Path.js
+++ packages/local/khusamov-svg/src/geometry/Path.js
@@ -26,6 +26,8 @@ Ext.define("Khusamov.svg.geometry.Path", {
 		"Khusamov.svg.geometry.path.Point"
 	],
 	
+	uses: ["Khusamov.svg.geometry.equation.Ray"],
+	
 	isPath: true,
 	
 	type: "path",
@@ -425,16 +427,21 @@ Ext.define("Khusamov.svg.geometry.Path", {
 	},
 	
 	/**
-	 * @param {Boolean} segmented Если равен true, то на выходе будет массив точек с 
+	 * Найти точки пересечения с другим геометрическим примитивом.
+	 * @param {Khusamov.svg.geometry.Primitive} primitive Фигура, с которой ищется пересечения.
+	 * @param {Object | Khusamov.svg.geometry.Point} [config] Дополнительные опции поиска пересечений или selPoint.
+	 * @param {Khusamov.svg.geometry.Point} [config.selPoint] Выбрать один делитель по точке.
+	 * @param {Boolean} [config.segmented] Если равен true, то на выходе будет массив точек с 
 	 * 1) индексом сегмента,
 	 * 2) координатой точки внутри сегмента (расстояние до точки от начала сегмента),
 	 * 3) координатой точки внутри пути (расстояние до точки от начала пути).
 	 * (Эта информация добавляется прямо в объект точки в свойство segment{index, distance, distanceByPath}).
+	 * @return {Khusamov.svg.geometry.Point[]}
 	 */
-	intersection: function(primitive, segmented) {
+	intersection: function(primitive, config) {
 		var type = "Path" + Ext.String.capitalize(primitive.type);
 		var intersection = Khusamov.svg.geometry.intersection[type];
-		return intersection.intersection(this, primitive, segmented);
+		return intersection.intersection(this, primitive, config);
 	},
 	
 	/**
@@ -446,6 +453,15 @@ Ext.define("Khusamov.svg.geometry.Path", {
 	split: function(primitive, selPoint) {
 		var splitter = Khusamov.svg.geometry.path.Splitter;
 		return splitter.split(this, primitive, selPoint);
+	},
+	
+	/**
+	 * Определение принадлежности точки многоугольнику.
+	 * Используется алгоритм "Учёт числа пересечений".
+	 */
+	contains: function(point) {
+		var ray = Ext.create("Khusamov.svg.geometry.equation.Ray", point, "0x");
+		return (this.intersection(ray) || []).length % 2 != 0;
 	}
 	
 });
